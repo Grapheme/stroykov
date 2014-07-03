43b0a7b84096dc585c20870816def3964d66d0ec
Artem Tumin <arttumin@gmail.com>
2014-07-03 19:01:29 +0400

    full-markup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+var App = (function(){
 
+	$(document).on('click', '.tabs > li', function(){
+
+		if( !$(this).hasClass('active') ) {
+			var allTabs = $('.tabs > li');
+			var tabCont = $('.tab');
+			var anchor = $(this).attr('id');
+
+			$('.tabs > li').removeClass('active');
+			$(this).addClass('active');
+			tabCont.removeClass('active').filter('[data-for="' + anchor + '"]').addClass('active');
+		}
+	});
+
+	$(document).on('click', '.details-dl > dt', function(){
+		$(this).parent().find('dd').removeClass('opened');
+		$(this).next().addClass('opened');
+	});
+
+})();
